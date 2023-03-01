b1f0595c0abb88c2bca832120772ea991767e2bd
Leon Windhorst <leon.windhorst@web.de>
2023-03-01 17:17:31 +0100

    enable border

--- themes/rose-pine/theme.lua
+++ themes/rose-pine/theme.lua
@@ -48,9 +48,9 @@ theme.fg_urgent     = palette.love
 theme.fg_minimize   = palette.love
 
 theme.useless_gap   = dpi(4)
-theme.border_width  = dpi(0)
+theme.border_width  = dpi(2)
 theme.border_normal = palette.background
-theme.border_focus  = palette.gold
+theme.border_focus  = palette.foam
 theme.border_marked = palette.foam
 
 -- For hotkey popup modifier keys
